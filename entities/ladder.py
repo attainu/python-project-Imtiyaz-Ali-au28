2263336586ee9ed18dffcdeff697a3349f04ed84
Imtiyaz-Ali-au28 <imtiyazaliq786@gmail.com>
2022-03-12 06:48:44 +0530

    Adding ladder functions/features

---
+++ entities/ladder.py
@@ -0,0 +1,22 @@
+class Ladder:
+    
+    def ladderCreate(text):
+        # while True:
+            try:
+                global ladder
+                ladder = {}
+                tempL = int(input(text))
+                if tempL <= 0:
+                    print("Ladder should be more than ZERO")
+                for i in range(1, tempL + 1):
+                    temp = list(map(int, input("Enter base & top position of the %s ladder: "%(i)).split()))
+                    ladder[temp[0]] = temp[1]    
+            except ValueError:
+                print("Enter number only !!\n")
+
+
+    def isLadder(position):
+        
+        if position in ladder:
+            return (position,ladder[position])
+ 
