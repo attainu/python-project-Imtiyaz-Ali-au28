f991dc86943d8808a58e1f54460e6192b1de1cc5
Imtiyaz-Ali-au28 <imtiyazaliq786@gmail.com>
2022-03-12 06:49:05 +0530

    Adding snake functions/features

---
+++ entities/snake.py
@@ -0,0 +1,26 @@
+class Snake:
+
+    def snakeCreate(text):
+
+        # while True:
+            try:
+                global snake
+                snake = {}
+                tempS = int(input(text))
+                if tempS <= 0:
+                    print("Snake should be more than ZERO")   
+                for i in range(1, tempS + 1):
+                    temp = list(map(int, input("Enter head & tail position of the %s snake: "%(i)).split()))
+                    snake[temp[0]] = temp[1]     
+            except ValueError:
+                print("Enter number only !!\n")
+
+
+    
+    def isSnake(position):
+        """ Checks if current position has snake or not 
+        """
+  
+        if position in snake:
+            return (position,snake[position])
+       
